cd1c62b84763d25b5368f6712f5b303eea6b287c
zhangyl <ylz20000319@gmail.com>
2026-05-05 02:21:00 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/drawstamputils', // 生产环境使用根路径
+  base: './', // 生产环境使用根路径
   build: {
     outDir: "dist", // Cloudflare Pages 默认使用 dist 目录
-    minify: 'esbuild', // 生产环境启用压缩
-    sourcemap: false // 生产环境不生成 sourcemap
+  
+   
   }
 })
 
